f85c8e27125c8f21e4e74aa7e4d7c986725be1d8
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2016-05-15 21:32:17 +0200

    Added selection highlight

--- src/main/scala/com/juanignaciosl/sidebyside/SideBySide.scala
+++ src/main/scala/com/juanignaciosl/sidebyside/SideBySide.scala
@@ -7,7 +7,7 @@ import org.scalajs.dom.raw.Node
 import org.scalajs.jquery.jQuery
 import org.scalajs.jquery.JQuery
 
-object SideBySide extends JSApp with CaretLocator{
+object SideBySide extends JSApp with CaretLocator {
   def main(): Unit = {
   }
 
@@ -16,11 +16,11 @@ object SideBySide extends JSApp with CaretLocator{
     val highlight = jQuery(document.createElement("pre"))
     val editor = jQuery(document.createElement("pre"))
     editor.attr("contenteditable", "true")
-    
+
     val originalContent = original.text()
     highlight.text(originalContent)
     editor.text(originalContent)
-    
+
     def highlightText = () => { updateCursorPosition(originalContent, highlight, editor) }
     jQuery(editor).keyup(highlightText)
     jQuery(editor).on("mouseup", highlightText)
@@ -28,22 +28,25 @@ object SideBySide extends JSApp with CaretLocator{
     editorContainer.append(highlight)
     editorContainer.append(editor)
   }
-  
+
   //INFO: passing `editor` shouldn't be necessary, because `event.srcElement` should be it, but it won't work.
   // In runtime `srcElement` is `undefined`. It can be retrieved through `originalEvent` but current jQuery facade
   // doesn't implement it.
   private def updateCursorPosition(originalContent: String, original: JQuery, editor: JQuery): Unit = {
-    val position = getCaretCharacterOffsetWithin(editor.get(0))
-    val content = originalContent.splitAt(position)
-    original.html("<span class=\"sbs-viewed\">" + content._1 + "</span>" + content._2)
+    val range = document.getSelection().getRangeAt(0)
+    val previous = originalContent.splitAt(range.startOffset)
+    val next = previous._2.splitAt(range.endOffset - range.startOffset)
+    original.html("<span class=\"sbs-viewed\">" + previous._1 + "</span>" + "<span class=\"sbs-selected\">" + next._1 + "</span>" + next._2)
   }
 
 }
 
 /**
- * Original JS source: 
+ * Original JS source:
  * - http://stackoverflow.com/questions/4811822/get-a-ranges-start-and-end-offsets-relative-to-its-parent-container/4812022#4812022
  * - http://jsfiddle.net/TjXEG/900/
+ *
+ * This is not actually needed for plain text.
  */
 trait CaretLocator {
   def getCaretCharacterOffsetWithin(node: Node): Int = {
